483af28293a8477d529587e73aa039ff6bb6523e
Ricardo Milan <ricardo@fluencypass.com>
2022-10-22 15:23:43 -0300

    player

---
+++ README.md
@@ -0,0 +1,3 @@
+## Referencias
+
+- [Make your first 2D platformer game](https://www.youtube.com/watch?v=xFEKIWpd0sU)
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -2,9 +2,10 @@ extends KinematicBody2D
 
 const UP = Vector2(0, -1)
 const GRAVITY = 20
-const MAXFALLSPEED = 200
-const MAXSPEED = 80
-const JUMPFORCE = 300
+const MAXFALLSPEED = 1000
+const MAXSPEED = 200
+const JUMPFORCE = 700
+const ACCELL = 50
 
 var motion = Vector2()
 
@@ -16,12 +17,14 @@ func _physics_process(delta):
 	if motion.y > MAXFALLSPEED:
 		motion.y = MAXFALLSPEED
 	
+	motion.x = clamp(motion.x, -MAXSPEED, MAXSPEED)
+	
 	if Input.is_action_pressed("right"):
-		motion.x = MAXSPEED
+		motion.x += ACCELL
 	elif Input.is_action_pressed("left"):
-		motion.x = -MAXSPEED
+		motion.x -= ACCELL
 	else:
-		motion.x = 0
+		motion.x = lerp(motion.x, 0, 0.2)
 	
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		motion.y = -JUMPFORCE
